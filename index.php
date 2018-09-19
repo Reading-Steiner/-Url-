4e732937478315f9e37663ec3b73c97de6a30b2a
Silence <483665176@qq.com>
2018-09-19 13:56:10 +0800

    PHP代码

---
+++ index.php
@@ -0,0 +1,23 @@
+<html>
+<head>
+<meta charset="utf-8"/>
+<title>短Url生成器</title>
+</head>
+<body>
+
+<form cation="" method="get" action="create.php">
+<p>长URL：<input type="url" name="longURL" id="longURL" size="100"></p>
+<p>
+-----------------------------------
+<input type="submit" value="提交"/>
+----------------
+<input type="reset" value="重置"/>
+-----------------------------------
+</p>
+
+</form>
+
+
+</body>
+</html>
+
